81496e5c0f80c23c2896e812d3b089b766e6f8b2
tabadines00 <thomas.abadines@gmail.com>
2024-04-16 15:39:48 -0700

    Ask GPT function finished and makes sure not to reply to itself

--- index.js
+++ index.js
@@ -62,43 +62,45 @@ async function getGPTAnswer(userMsg) {
       model: "gpt-3.5-turbo",
       max_tokens: 256
     });
-    console.log(chatCompletion.data.choices[0])
-    return chatCompletion.data.choices[0].text
+    console.log(chatCompletion.choices[0])
+    return chatCompletion.choices[0].message.content
   }
 
 // Bot actions
 
 client.on('messageCreate', async msg => {
     console.log(msg.content)
-    switch (msg.content.split(' ')[0]) {
-        // Hello!
-        case "Hello":
-            msg.reply(`Hello ${msg.author.username}`)
-            break
+    if(!message?.author.bot) {
+        switch (msg.content.split(' ')[0]) {
+            // Hello!
+            case "Hello":
+                msg.reply(`Hello ${msg.author.username}`)
+                break
 
-        // Ping replier
-        case "ping":
-            msg.reply("Pong!")
-            break
+            // Ping replier
+            case "ping":
+                msg.reply("Pong!")
+                break
 
-        // Duck Command to get a random duck
-        case "!duck":
-            msg.channel.send("Here's your duck!") //the reply to the user command
-            let duckImg = await getDuckPic() //fetches the URL from the API
-            msg.channel.send(duckImg) //sends the image URL to Discord
-            break
+            // Duck Command to get a random duck
+            case "!duck":
+                msg.channel.send("Here's your duck!") //the reply to the user command
+                let duckImg = await getDuckPic() //fetches the URL from the API
+                msg.channel.send(duckImg) //sends the image URL to Discord
+                break
 
-        // Command to get the next US Holiday
-        case "!nextUSHoliday":
-            let day = await getNextHoliday() //fetches the URL from the API
-            msg.channel.send("The next US Holiday is " + day)
-            break
+            // Command to get the next US Holiday
+            case "!nextUSHoliday":
+                let day = await getNextHoliday() //fetches the URL from the API
+                msg.channel.send("The next US Holiday is " + day)
+                break
 
-        // Command to ask ChatGPT a question
-        case "!askGPT":
-            let GPTreply = await getGPTAnswer(msg.content) //fetches the URL from the API
-            msg.channel.send(GPTreply)
-            break
+            // Command to ask ChatGPT a question
+            case "!askGPT":
+                let GPTreply = await getGPTAnswer(msg.content) //fetches the URL from the API
+                msg.channel.send(GPTreply)
+                break
+        }
     }
 })
 
